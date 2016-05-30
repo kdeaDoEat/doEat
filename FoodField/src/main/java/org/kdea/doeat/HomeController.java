434216d05ec56adc1a1e565722dd0204c4020772
vitakang <vitakang@naver.com>
2016-05-30 09:31:08 +0900

    hi

---
+++ FoodField/src/main/java/org/kdea/doeat/HomeController.java
@@ -0,0 +1,46 @@
+package org.kdea.doeat;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping(value="/")
+public class HomeController {
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "main", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		System.out.println("∏ﬁ¿Œ");
+		return "main";
+	}
+	
+	@RequestMapping(value = "notice" ,method = RequestMethod.GET)
+	public String notice(){
+		return "board/noticeBoard";
+	}
+	
+	@RequestMapping(value="free", method = RequestMethod.GET)
+	public String free(){
+		return "board/freeBoard";
+	}
+	
+	@RequestMapping(value="review", method = RequestMethod.GET)
+	public String review(){
+		return "board/reviewBoard";
+	}
+	
+	@RequestMapping(value = "select", method = RequestMethod.GET)
+	public String select(){
+		return "board/selectBoard";
+	}
+	
+}
