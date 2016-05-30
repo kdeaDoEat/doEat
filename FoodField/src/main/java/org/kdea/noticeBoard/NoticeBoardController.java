fa84089ffac1e1856edf6639781a42223f237b2b
duniv5-018 <duniv5-018@duniv5-018-PC>
2016-05-30 13:51:02 +0900

    commit

--- FoodField/src/main/java/org/kdea/noticeBoard/NoticeBoardController.java
+++ FoodField/src/main/java/org/kdea/noticeBoard/NoticeBoardController.java
@@ -1,5 +1,17 @@
 package org.kdea.noticeBoard;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("noticeboard")
 public class NoticeBoardController {
 
+	@RequestMapping("/main")
+	public String noticeMain(){
+		
+		return "board/noticeBoard";
+		
+	}
+	
 }
